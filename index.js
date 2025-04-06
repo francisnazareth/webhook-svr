e809d14ee41f81de33c158b185a0bad606fd4d31
Francis Nazareth <fnazaret@microsoft.com>
2025-04-06 12:56:20 +0300

    adding api-details page.

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get('/', (req, res) => {
     res.render('index', { messages });
 });
 
+//Serve the API details page. 
+
+app.get('/api-details', (req, res) => {
+    res.render('api-details', { messages });
+});
+
 // Handle (Webhook) POST requests
 app.post('/webhook', (req, res) => {
     try {
@@ -35,6 +41,35 @@ app.post('/webhook', (req, res) => {
         console.error('Error processing payload:', error.message);
         res.status(400).send({ status: 'error', message: error.message });
     }
+});
+
+//Handle form submission when API request access is clicked. 
+
+app.post('/request-access', (req, res) => {
+    console.log('API request button clicked!');
+    
+    var workflowURL = "https://prod-01.northcentralus.logic.azure.com:443/workflows/eea9e463159c48c1978a938c8413a382/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=I6bhk32ftNZZhJKSD00qgBqpF00GlmIz1Z6U4kF0JBg";
+    let jsonMessage = {};
+    jsonMessage.Requester = req.body.APIRequester; // Get the requester from the form
+    jsonMessage.APIRequested = req.body.APIRequested; // Get the API requested from the form
+    jsonMessage.Justification = req.body.Justification; // Get the justification from the form
+    jsonMessage.APIProvider = req.body.APIProvider; // Get the API provider from the form
+    jsonMessage.GDXApprover = "gdxadmin@qdf.gov.qa"; // Get the GDX approver from the form
+    jsonMessage.APIProviderEmail  = req.body.APIProviderEmail; // Get the callback URL from the form
+
+    console.log('JSON message:', jsonMessage);
+    
+
+    axios.post(workflowURL, jsonMessage)
+        .then(response => {
+            console.log('Response from callback URL:', response.data);
+        })
+        .catch(error => {
+            console.error('Error posting to callback URL:', error.message);
+        });
+
+    var requestMessage = "API request is submitted.";
+    res.render("api-details", { requestMessage }); // Render the updated messages
 });
 
 //Handle form submission when button is clicked. This is a POST request to the server when the button is clicked.
