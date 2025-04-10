a52f9a67fee9985afbb9948b7e3079a99da6e92a
Francis Nazareth <fnazaret@microsoft.com>
2025-04-10 14:47:19 +0300

    minor changes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const axios = require('axios');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
+var cors = require('cors');
+app.use(cors());
 
 //let message = 'No message received yet';
 let messages = [];
@@ -19,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.render('index', { messages });
 });
+app.get('/api-list', (req, res) => {
+    res.status(200).send({ status: true, messages });
+});
 
 //Serve the API details page. 
 
@@ -31,12 +37,14 @@ app.post('/webhook', (req, res) => {
     try {
         const payload = req.body;
 
-        let  message = payload; // Update the message
+        let message = payload; // Update the message
+        message.id = uuidv4();
+        message.updatedOn = new Date().getTime();
         messages.push(message); // Store the message in the array
-       
+
         console.log('Received payload:', message);
 
-        res.render("index",  { messages });
+        res.render("index", { messages });
     } catch (error) {
         console.error('Error processing payload:', error.message);
         res.status(400).send({ status: 'error', message: error.message });
@@ -47,18 +55,18 @@ app.post('/webhook', (req, res) => {
 
 app.post('/request-access', (req, res) => {
     console.log('API request button clicked!');
-    
-    var workflowURL = "https://prod-01.northcentralus.logic.azure.com:443/workflows/eea9e463159c48c1978a938c8413a382/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=I6bhk32ftNZZhJKSD00qgBqpF00GlmIz1Z6U4kF0JBg";
+
+    var workflowURL = "https://la-gdx-poc.azurewebsites.net:443/api/api-approval/triggers/When_a_HTTP_request_is_received/invoke?api-version=2022-05-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=bHgoXK0tnkobxKE0Gw0Fuh5d-NUqHF0VXyaYQuH04no";
     let jsonMessage = {};
     jsonMessage.Requester = req.body.APIRequester; // Get the requester from the form
     jsonMessage.APIRequested = req.body.APIRequested; // Get the API requested from the form
     jsonMessage.Justification = req.body.Justification; // Get the justification from the form
     jsonMessage.APIProvider = req.body.APIProvider; // Get the API provider from the form
     jsonMessage.GDXApprover = "gdxadmin@qdf.gov.qa"; // Get the GDX approver from the form
-    jsonMessage.APIProviderEmail  = req.body.APIProviderEmail; // Get the callback URL from the form
+    jsonMessage.APIProviderEmail = req.body.APIProviderEmail; // Get the callback URL from the form
 
     console.log('JSON message:', jsonMessage);
-    
+
 
     axios.post(workflowURL, jsonMessage)
         .then(response => {
@@ -75,7 +83,7 @@ app.post('/request-access', (req, res) => {
 //Handle form submission when button is clicked. This is a POST request to the server when the button is clicked.
 app.post('/approve-request', (req, res) => {
     console.log('Button clicked!:  ' + req.body.index);
-    let index = req.body.index; // Get the index of the message to approve
+    let index = messages.findIndex(msg => msg.id == req.body.index); // Get the index of the message to approve
     let message = messages[index]; // Get the message to approve
 
     //Add additional fields to the message. 
@@ -84,9 +92,9 @@ app.post('/approve-request', (req, res) => {
 
     callbackURL = message.callbackURL; // Get the callback URL from the message
     console.log('Callback URL:', callbackURL);
-   
+
     //post the JSON message to the callback URL.
- 
+
     axios.post(callbackURL, message)
         .then(response => {
             console.log('Response from callback URL:', response.data);
@@ -111,5 +119,5 @@ app.use((err, req, res, next) => {
 var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('Server is running on '+ port);
+    console.log('Server is running on ' + port);
 });
